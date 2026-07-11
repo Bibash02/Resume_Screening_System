9d84fc0f6380e56239cb92d59d5f8dd1bfe06843
Bibash Bayalkoti <bayalkotibibash@gmail.com>
2026-07-11 11:36:09 +0545

    update resume parser with pdf and docx allow to support also

--- utils/resume_parser.py
+++ utils/resume_parser.py
@@ -1,13 +1,40 @@
 import os
+import pdfplumber
+from docx import Document
 
 def extract_text_from_txt(file_path):
     with open(file_path, "r", encoding="utf-8", errors = "ignore") as f:
         return f.read()
+    
+def extract_text_from_pdf(file_path):
+    text = ""
+    with pdfplumber.open(file_path) as pdf:
+        for page in pdf.pages:
+            page_text = page.extract_text
+
+            if page_text:
+                text += page_text + "\n"
+
+def extract_text_from_docx(file_path):
+    document = Document(file_path)
+
+    text = []
+
+    for paragraph in document.paragraphs:
+        text.append(paragraph.text)
+    return "\n".join(text)
+
 
 def extract_resume_text(file_path):
     ext = os.path.splitext(file_path)[1].lower()
 
     if ext == ".txt":
         return extract_text_from_txt(file_path)
+    elif ext == ".pdf":
+        return extract_text_from_pdf(file_path)
+    elif ext == ".docx":
+        return extract_text_from_docx(file_path)
+    else:
+        raise ValueError("Unsupported file format.")
     
     raise ValueError("Unsupported file format. Use .txt for now.")
